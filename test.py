7a2d6ffff4b517470aa92049e56ad13aed1b9760
lucasvberkel <lucasvberkel@gmail.com>
2016-02-23 11:13:47 +0100

    test

--- test.py
+++ test.py
@@ -1,12 +1,33 @@
-def createSmoothedN1Dict(seq_dict):
-	uniGramDict = {}
-	for element in seq_dict:
-		words = element.split(" ")
-		if words[0] in uniGramDict:
-			uniGramDict[words[0]] += seq_dict[element]
-		else:
-			uniGramDict[words[0]] = seq_dict[element]
-	return uniGramDict
 
-poep = {"key hoi": 1, "key praat": 2, "computer praat": 2, "plaatje Praat": 2, "Leuk leuk": 2}
-print(createSmoothedN1Dict(poep))
+def convert_txt_to_sentencelist(corpus, n):
+	n-=1
+	sentencelist = []
+	with open(corpus) as data_file:
+		sentence = ["<s>"] * n
+		for line in data_file:
+			if line != "======================================\n":
+				if line == "./. \n":
+					for i in range(n):
+						sentence.append("</s>")	
+					sentencelist.append(sentence)
+					continue
+				for word in line.split(" "):
+					if word.endswith("\n"):
+						word = word.replace("\n", "")
+					if word == "./.":
+						for i in range(n):
+							sentence.append("</s>")	
+						sentencelist.append(sentence)
+						sentence = ["<s>"] * n
+					else:
+						if word != "" and word[0].isalnum() :
+							sentence.append(word)
+			else:
+				sentence = ["<s>"] * n
+	return sentencelist
+
+if __name__ == "__main__":
+	corpus = "test.pos"
+	sentencelist = convert_txt_to_sentencelist(corpus, 2)
+	for x in range(len(sentencelist)):
+		print(sentencelist[x])
