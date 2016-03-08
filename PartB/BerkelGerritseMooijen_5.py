710641ad148da64f293dd6aa6e08da2b3ed50476
lucasvberkel <lucasvberkel@gmail.com>
2016-03-08 12:46:40 +0100

    Binarize

--- PartB/BerkelGerritseMooijen_5.py
+++ PartB/BerkelGerritseMooijen_5.py
@@ -34,9 +34,30 @@ def convert_txt_to_sentencelist(non_binarized_file):
 # 	binarizedList
 # 	for sentence in sentencelist
 # 		print
+def binarize(sentence):
+	length = len(sentence)
+	# print(sentence[0])
+	if isinstance(sentence[1], str) :
+		return sentence
+	sentence[1] = binarize(sentence[1])
+	if length == 2:
+		return sentence
+	if length > 3:
+		name = sentence[0]
+		if "|" in name:
+			name = name + "_" + sentence[1][0]
+		else:
+			name =  "@" + name + "|" + sentence[1][0]
+		sentence[2] = [name] + sentence[2:]
+	sentence[2] = binarize(sentence[2])
+	del sentence[3:]
+	return sentence
 
 if __name__ == "__main__":
 	non_binarized_file = args.input
 	binarized_file = args.output
 	sentencelist = convert_txt_to_sentencelist(non_binarized_file)
-	# binarizedlist = binarize(sentencelist)
+	binarizedlist = []
+	for sentence in sentencelist:
+		binarizedlist.append(binarize(sentence))
+	print(binarizedlist)
