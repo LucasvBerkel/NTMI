e40d683a81404cd3755c8166d6b96a165d7afb68
RubenGerritse <rgerritse@live.nl>
2016-03-08 11:15:28 +0100

    toList

--- PartB/BerkelGerritseMooijen_5.py
+++ PartB/BerkelGerritseMooijen_5.py
@@ -7,6 +7,7 @@ parser.add_argument("-input", help="path to non-binarized file", type=str)
 parser.add_argument("-output", help="path to binarized file", type=str)
 args = parser.parse_args()
 
+# Converts the textfile to a list
 def convert_txt_to_sentencelist(non_binarized_file):
 	sentencelist = []
 	with open(non_binarized_file) as data_file:
@@ -14,33 +15,28 @@ def convert_txt_to_sentencelist(non_binarized_file):
 			if line != '\n':
 				line = line.replace('\n', '')
 				terminalList = re.findall("\(\S+\s", line)
-				print(terminalList)
 				for terminal in terminalList:
-					print(terminal)
-					temp = re.compile(terminal)
-					word = re.compile("\[\'\S+\'")
-					
-					line = temp.sub(word, line)
-				# print(line)
-				# print(temp)
-					# line = line.replace(terminal, word)
-				# print(line)
-				# line = line.replace(' ', ',')
-				# line = line.replace('(', '[')
-				# line = line.replace(')', ']')
-				# print(line)
-				# terminalList = re.findall("\s[\w|.]+\s", line)
-				# for terminal in terminalList:
-					# line = line.replace(terminal, '\"{}\"'.format(terminal.strip()))
-				# line = line.strip()
-				# sentence = ast.literal_eval(line)
-				# sentencelist.append(sentence)
-	# return sentencelist
+					word = re.findall("[^(\s+]+", terminal)
+					substitute = "[\'{}\' ".format(word[0])
+					line = line.replace(terminal, substitute)
+				terminalList = re.findall("\s[^\s)]+\)", line)
+				for terminal in terminalList:
+					word = re.findall("[^)\s+]+", terminal)
+					substitute = " \'{}\']".format(word[0])
+					line = line.replace(terminal, substitute)
+				line = line.replace(' ', ',')
+				line = line.replace(')', ']')
+				sentence = ast.literal_eval(line)
+				sentencelist.append(sentence)
+	return sentencelist
 			
+# def binarize(sentencelist):
+# 	binarizedList
+# 	for sentence in sentencelist
+# 		print
 
 if __name__ == "__main__":
 	non_binarized_file = args.input
 	binarized_file = args.output
 	sentencelist = convert_txt_to_sentencelist(non_binarized_file)
-	# for sentence in sentencelist:
-		# print(sentence)
+	# binarizedlist = binarize(sentencelist)
